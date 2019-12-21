7cd25ad81b77b5b4349d945ce6505f097e48f85d
Hyeeun <hyeeuni.mok@gmail.com>
2019-12-21 10:17:56 -0500

    delete function

--- app.js
+++ app.js
@@ -63,6 +63,26 @@ var budgetController = (function() {
       return newItem;
     },
 
+    deleteItem: function(type, id) {
+      var ids, index;
+      
+      // id = 6
+      //data.allItems[type][id];
+      // ids = [1 2 4  8]
+      //index = 3
+      
+      ids = data.allItems[type].map(function(current) {
+          return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+          data.allItems[type].splice(index, 1);
+      }
+      
+  },
+
     calculateBudget: function() {
       // Calculate total income and expenses
       calculateTotal('exp');
@@ -144,6 +164,12 @@ var UIController = (function() {
 
       // Insert the HTML into the Dom
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+
+    deleteListItem: function(selectorID) {
+
+      var el =  document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
     },
 
     clearFields: function() {
@@ -233,21 +259,24 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   var ctrlDeleteItem = function(event) {
     var itemID, splitID, type, ID;
-
-    itemID = event.target.parentNode.parentNode.parentNode.id;
-
+    
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+    
     if (itemID) {
+        
       //inc-1
       splitID = itemID.split('-');
       type = splitID[0];
-      ID = splitID[1];
-
-      // 1. Delete the item from the data structure
-
+      ID = parseInt(splitID[1]);
+      
+      // 1. delete the item from the data structure
+      budgetCtrl.deleteItem(type, ID);
+      
       // 2. Delete the item from the UI
+      UICtrl.deleteListItem(itemID);
 
       // 3. Update and show the new budget
-      
+      updateBudget();
     }
 
   }
